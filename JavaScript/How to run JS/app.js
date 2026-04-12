ea9bdf78c652fad7d79676b5e0aa4fa5e42896b4
utkarsh1906-keshari <utkarshvns1906@gmail.com>
2026-04-12 23:37:43 +0530

    experiment2

---
+++ JavaScript/How to run JS/app.js
@@ -0,0 +1,24 @@
+// console.log("hello from JavaScript")
+// function showAlert(){
+//     alert("Hello CSE 22")
+// }
+
+var name = "John doe";
+var age = 30;
+var isEmployed= true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+var age = 40;
+console.log(age);
+if(true){
+    let age = 30;
+}
+console.log(age);
+
+//  string concatenation
+let greeting = "Hello,"+name+"! You are" + age + "years old"
+console.log(greeting);
+// string literals    best hai ye as compared to string literals
+let message = `Hello ${name}, you are ${age} years old.`;
+console.log(message);
---
+++ JavaScript/How to run JS/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+var name = "John doe";
+var age = 30;
+var isEmployed= true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+var age = 40;
+console.log(age);
